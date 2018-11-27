88b647ac4b507119e609eebf85faea1bcd9029ba
Julian reyes Escrigas <julian.reyes.escrigas@gmail.com>
2018-11-27 02:15:34 -0500

    lint: arreglado espaciado

--- src/lib/repository/memory-repo.spec.ts
+++ src/lib/repository/memory-repo.spec.ts
@@ -1,19 +1,19 @@
 import {MemoryRepo} from "./memory-repo";
 
 test("Allow create data", async () => {
-   const repo = new MemoryRepo();
+    const repo = new MemoryRepo();
 
-   const value = await repo.create({name: "Julian"});
-   expect(value.id).toBe(1);
-   expect(value.name).toBe("Julian");
+    const value = await repo.create({name: "Julian"});
+    expect(value.id).toBe(1);
+    expect(value.name).toBe("Julian");
 });
 
 test("Allow create more that one data", async () => {
-   const repo = new MemoryRepo();
-   const value1 = await repo.create({name: "Hello"});
-   const value2 = await repo.create({name: "World"});
-   expect(value1.id).toBe(1);
-   expect(value2.id).toBe(2);
+    const repo = new MemoryRepo();
+    const value1 = await repo.create({name: "Hello"});
+    const value2 = await repo.create({name: "World"});
+    expect(value1.id).toBe(1);
+    expect(value2.id).toBe(2);
 });
 
 test("Allow update previously created data", async () => {
