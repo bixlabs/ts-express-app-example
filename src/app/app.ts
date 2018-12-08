4d903b9ff9e0948dfab16a45fe5609c8303cc319
Julian reyes Escrigas <julian.reyes.escrigas@gmail.com>
2018-12-08 14:05:13 -0500

    add handlebars and server view support

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,11 @@
 import bodyParser from "body-parser";
 import express from "express";
+import handlebars from "express-handlebars";
 import {expressLogger} from "./logger";
 
 const app = express();
-
+app.engine("handlebars", handlebars({defaultLayout: "main"}));
+app.set("view engine", "handlebars");
 app.use(expressLogger);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
